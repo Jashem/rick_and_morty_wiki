35b6f7f3bc17228dcb7a6673cd32b19510f02494
Mohammad Jashem <mjashem@hotmail.com>
2024-04-03 11:28:04 +0600

    add icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/application/app_bloc_observer.dart';
 import 'core/presenation/app_widget.dart';
 import 'core/shared/dependency_injection.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
+  WidgetsFlutterBinding.ensureInitialized();
   setupDI();
   Bloc.observer = AppBlocObserver();
   runApp(const AppWidget());
